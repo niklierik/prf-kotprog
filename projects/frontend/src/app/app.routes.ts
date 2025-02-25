bbfaeddb574b92f8f7509dae5cc48f549258eaec
niklierik <erik200203@gmail.com>
2025-02-25 10:17:20 +0100

    Generate empty pages

--- projects/frontend/src/app/app.routes.ts
+++ projects/frontend/src/app/app.routes.ts
@@ -1,10 +1,35 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ComposeComponent } from './pages/compose/compose.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'article/:id',
+    component: ArticleComponent,
+  },
+  {
+    path: 'compose',
+    component: ComposeComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
   },
   {
     path: '**',
---
+++ projects/frontend/src/app/pages/compose/compose.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compose',
+  imports: [],
+  templateUrl: './compose.component.html',
+  styleUrl: './compose.component.scss'
+})
+export class ComposeComponent {
+
+}
