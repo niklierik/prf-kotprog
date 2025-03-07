c7447183c8d059ae891e60675bb32572b99c5760
niklierik <erik200203@gmail.com>
2025-03-07 23:26:57 +0100

    Install ngx-markdown and setup demo component

--- projects/frontend/src/app/pages/article/article.component.ts
+++ projects/frontend/src/app/pages/article/article.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [CommonModule, MarkdownModule],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+  styleUrl: './article.component.scss',
 })
-export class ArticleComponent {
-
-}
+export class ArticleComponent {}
--- projects/frontend/src/app/pages/compose/compose.component.ts
+++ projects/frontend/src/app/pages/compose/compose.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-compose',
   imports: [],
   templateUrl: './compose.component.html',
-  styleUrl: './compose.component.scss'
+  styleUrl: './compose.component.scss',
 })
-export class ComposeComponent {
-
-}
+export class ComposeComponent {}
