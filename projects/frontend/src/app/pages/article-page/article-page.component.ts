f32226618d9b049eaf5dda59faaee7a4db4bcf48
niklierik <erik200203@gmail.com>
2025-04-06 10:46:57 -0400

    Start gallery feature

--- projects/frontend/src/app/pages/article-page/article-page.component.ts
+++ projects/frontend/src/app/pages/article-page/article-page.component.ts
@@ -1,8 +1,15 @@
-import { Component, effect, Resource, Signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  Resource,
+  signal,
+  Signal,
+} from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ArticleInfo } from '@kotprog/common';
+import { ArticleInfo, PermissionLevel } from '@kotprog/common';
 import { ArticleComponent } from './article/article.component';
 import { ArticleSuggestionComponent } from './article-suggestion/article-suggestion.component';
 import { Title } from '@angular/platform-browser';
@@ -10,6 +17,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ArticlePageResources } from './article-page.resources';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-article-page',
@@ -26,6 +34,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ArticlePageComponent {
   public readonly articleId: Signal<string | undefined>;
+  public readonly canEditArticle: Signal<boolean>;
 
   public readonly data: Resource<string | undefined>;
 
@@ -38,6 +47,7 @@ export class ArticlePageComponent {
     articlePageResources: ArticlePageResources,
     router: Router,
     titleService: Title,
+    authService: AuthService,
   ) {
     const idObsv = activatedRoute.params.pipe(
       map((params) => params['id'] || undefined),
@@ -51,6 +61,21 @@ export class ArticlePageComponent {
     this.articles = articlePageResources.createArticleSuggestionsSignal(
       this.info.value,
     );
+
+    this.canEditArticle = computed(() => {
+      const payload = authService.payload();
+
+      if (!payload) {
+        return false;
+      }
+
+      const permissionLevel = payload.permissionLevel || PermissionLevel.USER;
+
+      return (
+        this.info.value()?.author.id === payload.email ||
+        permissionLevel >= PermissionLevel.ADMIN
+      );
+    });
 
     effect(() => {
       const title = this.info.value()?.title;
